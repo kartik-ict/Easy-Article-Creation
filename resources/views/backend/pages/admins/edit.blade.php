927f6a12156b798fb414a2f905bdcc9deca90daf
kartik-ict <kartik@icreativetechnologies.com>
2025-04-30 16:49:34 +0530

    solved bugs in create/edit admin user.

--- resources/views/backend/pages/admins/edit.blade.php
+++ resources/views/backend/pages/admins/edit.blade.php
@@ -84,15 +84,15 @@
                                 <label for="username">{{ __('admins.form.username') }}</label>
                                 <input type="text" class="form-control" id="username" name="username" placeholder="{{ __('admins.form.placeholder.username') }}" required value="{{ $admin->username }}">
                             </div>
-                            <div class="form-group col-md-6 col-sm-6 px-2">
-                                <label for="ip_address">@lang('admins.form.ip_address')</label>
-                                <input type="text"
-                                       class="form-control"
-                                       id="ip_address"
-                                       name="ip_address"
-                                       value="{{ old('ip_address', $admin->ip_address ?? '') }}"
-                                       placeholder="@lang('admins.form.placeholder.ip_address')">
-                            </div>
+                        </div>
+                        <div class="form-group col-md-6 col-sm-6 px-2">
+                            <label for="ip_address">@lang('admins.form.ip_address')</label>
+                            <input type="text"
+                                   class="form-control"
+                                   id="ip_address"
+                                   name="ip_address"
+                                   value="{{ old('ip_address', $admin->ip_address ?? '') }}"
+                                   placeholder="@lang('admins.form.placeholder.ip_address')">
                         </div>
                         <button type="submit" class="btn btn-primary mt-4 pr-4 pl-4">{{ __('admins.save_admin') }}</button>
                     </form>
